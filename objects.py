8d75f7176478a8e71a3d583bdd662e7319381065
Alexander Starr <AlexanderLStarr@gmail.com>
2013-10-06 13:11:08 -0500

    Added many baseline concentrations. Implemented environmental partitioning.

--- objects.py
+++ objects.py
@@ -1,15 +1,73 @@
 import random
 
-# Blank concentrations dictionary
-concs = {'H+':0,
-         'CO2':0,
-         'O2':0,
-         'Glc':0,
-         'Fru':0,
-         'Lac':0,
-         'AAs':0,
-         'N':0,
-         'P':0}
+# A base environmental concentrations dictionary.
+# Emulates the laboratory standard LB medium.
+# http://en.wikipedia.org/wiki/Lysogeny_broth
+ENVC = {'H+': 10**-7.4,
+        'CO2': 0,
+        'O2': 0,
+        'Glc': 3*10**-3,
+        'Fru': 0,
+        'Lac': 0,
+        'AAs': 9.6*10**-2,
+        'N': 0,
+        'P': 0,
+        'NaCl': 3*10**-3}
+
+# A base internal cellular concentrations dictionary.
+# Each resource has a dictionary containing min/max/current values.
+# All values are in mol/L and are based on the following research article:
+# http://www.ncbi.nlm.nih.gov/pmc/articles/PMC2754216/
+CELLC = {'H+':  {'current': 10**-7.5,
+                 'minLive': 10**-7.0,
+                 'minGrow': 10**-7.2,
+                 'maxGrow': 10**-7.8,
+                 'maxLive': 10**-8.0},
+         'ATP': {'current': 8*10**-3,
+                 'minLive': 5*10**-4,
+                 'minGrow': 2*10**-3,
+                 'maxGrow': 10**-2,
+                 'maxLive': 2*10**-2},
+         'CO2': {'current': 0,
+                 'minLive': 0,
+                 'minGrow': 0,
+                 'maxGrow': 10**-3,
+                 'maxLive': 10**-2},
+         'O2':  {'current': 0,
+                 'minLive': 0,
+                 'minGrow': 0,
+                 'maxGrow': 10**-3,
+                 'maxLive': 10**-2},
+         'Glc': {'current': 8*10**-3,
+                 'minLive': 0,
+                 'minGrow': 10**-3,
+                 'maxGrow': 10**-2,
+                 'maxLive': 2*10**-2},
+         'Fru': {'current': 0,
+                 'minLive': 0,
+                 'minGrow': 0,
+                 'maxGrow': 10**-2,
+                 'maxLive': 2*10**-2},
+         'Lac': {'current': 0,
+                 'minLive': 0,
+                 'minGrow': 0,
+                 'maxGrow': 10**-2,
+                 'maxLive': 2*10**-2},
+         'AAs': {'current': 1.5*10**-1,
+                 'minLive': 10**-2,
+                 'minGrow': 10**-1,
+                 'maxGrow': 2*10**-1,
+                 'maxLive': 3*10**-1},
+         'N':   {'current': 0,
+                 'minLive': 0,
+                 'minGrow': 0,
+                 'maxGrow': 10**-2,
+                 'maxLive': 10**-1},
+         'P':   {'current': 0,
+                 'minLive': 0,
+                 'minGrow': 0,
+                 'maxGrow': 10**-2,
+                 'maxLive': 10**-1}}
 
 
 
@@ -26,11 +84,26 @@ class Environment:
     def __str__(self):
         return self.name
 
-    def remove():
-        return conc
+    # Returns a list of dictionaries, one for each organism in the community.
+    # Each dictionary contains the moles of resources available to that organism
+    # for the current step.
+    def partition(self, community):
+        # Find the proportion of each organism by volume
+        volumes = [org.volume() for org in community.organisms]
+        total = sum(volumes)
+        proportions = [ind/total for ind in volumes]
 
-    def add():
-        return conc
+        # Then build a dictionary for each organism, giving it resources
+        # proportional to its fractional volume.
+        dList = [{} for org in proportions]
+        for i in range(len(dList)):
+            for key in self.conc:
+                dList[i][key] = self.conc[key] * proportions[i]
+        return dList
+
+    # Takes 
+    def update(self, ):
+        return 
 
 
 class Community:
@@ -57,11 +130,8 @@ class Organism:
     def volume(self):
         return self.cellVolume * self.count
 
-    def uptake(self, env):
+    def exchange(self, conc):
         return 0
-
-    def expel(self):
-        return
 
 
 class Operon:
@@ -77,4 +147,9 @@ class Operon:
 operons = {}
 
 # Default organisms
-eColi = Organism('E. coli', [], {'atp':100})
+eColi = Organism('E. coli', [], CELLC)
+cDiff = Organism('C. diff', [], CELLC, 200)
+comm = Community([eColi,cDiff])
+print comm.organisms
+env = Environment('Lab', 1, ENVC, 37, True)
+print env.partition(comm)
