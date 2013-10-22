b967899fa87c2516512437f543c75a1c3f18871d
Alexander Starr <AlexanderLStarr@gmail.com>
2013-10-22 14:37:15 -0500

    Beginning on the GUI of the game.  Just draws a background for now.

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
-import pygame
-import random
-import sys
+import pygame, random, sys, os
 from pygame.locals import *
-
 from defs.defaults import *
 
-env = Environment('Lab', 1, ENVR)
+# Initialize
+IMG_DIR = os.path.join('data', 'img')
+screen = pygame.display.set_mode((800,600))
+background = pygame.image.load(os.path.join(IMG_DIR, 'background.png'))
+screen.blit(background, (0,0))
+
+'''env = Environment('Lab', 1, ENVR)
 eco = Ecosystem([eColi], env)
 for i in range(1440):
     eco.cycle()
 
-print ""
+print'''
+
+class Game():
+    def __init__(self):
+        self.high_score = 0
+
+pygame.init()
+statusFont = pygame.font.Font(None, 17)
+black = (0,0,0)
+clock = pygame.time.Clock()
+pygame.display.set_caption('The Organism Trail')
+FPS = 30
+
+while True:
+    pygame.display.flip()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
